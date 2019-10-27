76152b931a553ab03eda5b5c45285d2291d67a3a
Hans Foster <fosterhans@gmail.com>
2019-10-27 18:28:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(collection)
+  i = 0 
+  while i < collection.size
+    yield(collection[i])
+    i += 1 
+  end
+  collection
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
